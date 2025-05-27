5291f2aea01181fd89bc538fa848e51e8af38d88
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-05-27 16:57:54 +0530

    feat(google-auth): Added the feature to authenticate with google

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
+const PORT = process.env.PORT;
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -17,3 +18,49 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => (null, user));
+
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'>Log-In with Google</a>");
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+app.get("/profile", (req, res) => {
+  res.send(`Welcome ${req.user.displayName}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`App is Listening on ${PORT}`);
+});
